e9439e2e93a775b56f48fc3dff283cbc5e6dba6b
Pedro Zago <pedro.camargozago@hotmail.com.br>
2022-05-23 16:22:56 -0300

    Aula 01

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { TouchableOpacity, View, Image, Text, TouchableOpacityProps } from 'react-native';
+
+import DiscordImg from '../../assets/discord.png';
+import { styles } from './styles';
+
+type ButtonIconProps = TouchableOpacityProps & {
+  title: string;
+}
+
+export const ButtonIcon = ({ title, ...rest }: ButtonIconProps) => {
+  return (
+    <TouchableOpacity style={styles.container} {...rest}>
+      <View style={styles.iconWrapper}>
+        <Image source={DiscordImg} style={styles.icon} />
+      </View>
+
+      <Text style={styles.title}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
